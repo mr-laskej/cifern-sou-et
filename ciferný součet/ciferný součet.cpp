123038a01fb5a7f7325a7e872eacd89167a0cbfb
Matyáš Laský <matyas.lasky.020@pslib.cz>
2022-06-20 15:23:00 +0200

    Přidejte soubory projektu.

---
+++ ciferný součet/ciferný součet.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int soucet = 0;
+    int cislo;
+
+    printf("Zadej cislo pro ciferny soucet: ");
+    scanf("%d", &cislo);
+    while (cislo > 0)
+    {
+        soucet += cislo % 10;
+        cislo /= 10;
+    }
+    printf("Ciferny soucet cisla je: %d \n", soucet);
+}
+
